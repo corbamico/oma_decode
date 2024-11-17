7609a6976c351d36bec6e7711c56d1b26a15aca1
corbamico <corbamico@163.com>
2024-11-17 10:32:03 +0100

    fix naming rule, simplify using

--- oma_decode/Program.cs
+++ oma_decode/Program.cs
@@ -5,7 +5,7 @@ namespace oma_decode
 
     public class Decoding
     {
-        static byte[] _map = System.Text.ASCIIEncoding.GetEncoding(0).GetBytes("JaGuHAoLCqWXNbZAyWp");
+        static readonly byte[] _map = System.Text.ASCIIEncoding.GetEncoding(0).GetBytes("JaGuHAoLCqWXNbZAyWp");
 
 
         public static bool Decode(string strFileName, string strOutFile)
@@ -14,19 +14,18 @@ namespace oma_decode
             var magic = "01-45-4B-02-1A";
             var buffer = new byte[19 * 100];
             int nRead;
-            using (var outfs = System.IO.File.OpenWrite(strOutFile))
-            using (var fs = System.IO.File.OpenRead(strFileName))
+            using var outfs = System.IO.File.OpenWrite(strOutFile);
+            using var fs = System.IO.File.OpenRead(strFileName);
+            //file should be begin with '01 45 4B 02 1A'
+            //fs.Read(buffer, 0, 5);
+            fs.ReadExactly(buffer, 0, 5);
+            if (magic != BitConverter.ToString(buffer[0..5]))
             {
-                //file should be begin with '01 45 4B 02 1A'
-                fs.Read(buffer, 0, 5);
-                if (magic != BitConverter.ToString(buffer[0..5]))
-                {
-                    return false;
-                }
-                while ((nRead = fs.Read(buffer, 0, 19 * 100)) > 0)
-                {
-                    outfs.Write(decode_bytes(buffer[0..nRead]));
-                }
+                return false;
+            }
+            while ((nRead = fs.Read(buffer, 0, 19 * 100)) > 0)
+            {
+                outfs.Write(Decode_bytes(buffer[0..nRead]));
             }
             return true;
         }
@@ -36,33 +35,32 @@ namespace oma_decode
             var magic = "01-45-4B-02-1A";
             var buffer = new byte[19 * 100];
             int nRead;
-            using (var fs = System.IO.File.OpenRead(strFileName))
+            using var fs = System.IO.File.OpenRead(strFileName);
+            //file should be begin with '01 45 4B 02 1A'
+            //fs.Read(buffer, 0, 5);
+            fs.ReadExactly(buffer, 0, 5);
+            if (magic != BitConverter.ToString(buffer[0..5]))
             {
-                //file should be begin with '01 45 4B 02 1A'
-                fs.Read(buffer, 0, 5);
-                if (magic != BitConverter.ToString(buffer[0..5]))
-                {
-                    Console.WriteLine("[error] {0} is not valid oma file.", strFileName);
-                    return;
-                }
-                while ((nRead = fs.Read(buffer, 0, 19 * 100)) > 0)
-                {
-                    Console.Write(System.Text.Encoding.ASCII.GetString(decode_bytes(buffer[0..nRead])));
-                }
+                Console.WriteLine("[error] {0} is not valid oma file.", strFileName);
+                return;
+            }
+            while ((nRead = fs.Read(buffer, 0, 19 * 100)) > 0)
+            {
+                Console.Write(System.Text.Encoding.ASCII.GetString(Decode_bytes(buffer[0..nRead])));
             }
         }
 
-        static byte[] decode_bytes(byte[] bytes)
+        static byte[] Decode_bytes(byte[] bytes)
         {
             for (int i = 0; i < bytes.Length; i++)
             {
                 var index = i % _map.Length;
-                bytes[i] = decode_byte(bytes[i], index);
+                bytes[i] = Decode_byte(bytes[i], index);
             }
             return bytes;
         }
 
-        static byte decode_byte(byte c, int index)
+        static byte Decode_byte(byte c, int index)
         {
             if (c >= _map[index])
             {
@@ -74,25 +72,23 @@ namespace oma_decode
     }
     public class Encoding
     {
-        static byte[] _map = System.Text.ASCIIEncoding.GetEncoding(0).GetBytes("JaGuHAoLCqWXNbZAyWp");
+        static readonly byte[] _map = System.Text.ASCIIEncoding.GetEncoding(0).GetBytes("JaGuHAoLCqWXNbZAyWp");
         public static bool Encode(string strFileName, string strOutFile)
         {
-            byte[] magic = { 0x01, 0x45, 0x4B, 0x02, 0x1A };
+            byte[] magic = [0x01, 0x45, 0x4B, 0x02, 0x1A];
             var buffer = new byte[19 * 100];
             int nRead;
-            using (var outfs = System.IO.File.OpenWrite(strOutFile))
-            using (var infs = System.IO.File.OpenRead(strFileName))
+            using var outfs = System.IO.File.OpenWrite(strOutFile);
+            using var infs = System.IO.File.OpenRead(strFileName);
+            //file should be begin with '01 45 4B 02 1A'
+            outfs.Write(magic, 0, magic.Length);
+            while ((nRead = infs.Read(buffer, 0, 19 * 100)) > 0)
             {
-                //file should be begin with '01 45 4B 02 1A'
-                outfs.Write(magic, 0, magic.Length);
-                while ((nRead = infs.Read(buffer, 0, 19 * 100)) > 0)
-                {
-                    outfs.Write(encode_bytes(buffer[0..nRead]));
-                }
+                outfs.Write(Encode_bytes(buffer[0..nRead]));
             }
             return true;
         }
-        static byte encode_byte(byte c, int index)
+        static byte Encode_byte(byte c, int index)
         {
             int result;
             result = c + _map[index];
@@ -102,22 +98,12 @@ namespace oma_decode
             }
             return (byte)(result - 0x7F);
         }
-        static byte[] encode_bytes(byte[] bytes)
+        static byte[] Encode_bytes(byte[] bytes)
         {
             for (int i = 0; i < bytes.Length; i++)
             {
                 var index = i % _map.Length;
-                bytes[i] = encode_byte(bytes[i], index);
-            }
-            return bytes;
-        }
-        static byte[] encode_string(string str)
-        {
-            byte[] bytes = System.Text.ASCIIEncoding.GetEncoding(0).GetBytes(str);
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                var index = i % _map.Length;
-                bytes[i] = encode_byte(bytes[i], index);
+                bytes[i] = Encode_byte(bytes[i], index);
             }
             return bytes;
         }
@@ -136,9 +122,9 @@ namespace oma_decode
                 Encoding.Encode(args[1], args[2]);
                 return;
             }
-            usage();
+            Usage();
         }
-        static void usage()
+        static void Usage()
         {
             var lines = @"usage: oma_decode [-d <file.oma>|-e <file.omf> <file.oma>]";
             Console.WriteLine(lines);
